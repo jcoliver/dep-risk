a46a62dc3794ecfe6fe6293a198ff072f602638a
Jeff Oliver <jcoliver@email.arizona.edu>
2021-03-22 16:18:12 -0700

    More levaan testing and mock data creation

--- lavaan-testing.R
+++ lavaan-testing.R
@@ -45,27 +45,95 @@ Data2 <- data.frame(X = X,
                     M2 = M2)
 
 double_model <- '
-# The full model?
+# Define the full model, which includes direct effect (X) and covariate (P1)
   Y ~ b1 * M1 + b2 * M2 + c * X + P1
-# direct effect (implied by full model?)
-#  Y ~ c * X + P1
-# mediator
-  # regress mediators on X, including P as covariate
-  M1 ~ a1 * X + P1
-  M2 ~ a2 * X + P1
-  # regress Y on mediators
-  # Y ~ b1 * M1
-  # Y ~ b2 * M2
-# indirect effect (a*b)
+# Regress mediators on X, including P as covariate
+  M1 ~ a1 * X
+  M2 ~ a2 * X
+# Define additional parameters of interest to print
+# indirect effects (a*b)
   ab1 := a1 * b1
   ab2 := a2 * b2
+# direct effect (not defining, as it comes out in Regressions section)
+#  direct := c
 # total effect
   total := c + (a1 * b1) + (a2 * b2)
-# direct effect
-  direct := c
-# covariance between mediators (skipping)
-#  M1 ~~ M2
+# If desired and warranted, can add covariance between variables
+# M1 ~~ M2
 '
 
 double_fit <- sem(double_model, data = Data2)
 summary(double_fit)
+
+# Annotated output ($$) from summary:
+
+# lavaan 0.6-8 ended normally after 16 iterations
+# 
+# Estimator                                         ML
+# Optimization method                           NLMINB
+# Number of model parameters                        11
+# 
+# Number of observations                           100
+# 
+# Model Test User Model:
+#   
+#   Test statistic                                 0.677
+# Degrees of freedom                                 1
+# P-value (Chi-square)                           0.410
+# 
+# Parameter Estimates:
+#   
+#   Standard errors                             Standard
+# Information                                 Expected
+# Information saturated (h1) model          Structured
+# 
+# $$ Regressions section includes coefficient estimate for individual paths
+# $$ for example, the indirect effects via mediator 1, M1, are the a1 and b1 
+# $$ paths. Each estimate is shown in the corresponding regression model 
+# $$ section.
+#
+# $$ Regression on Y (first section) shows 
+# $$    M1: effect (b path) of mediator 1 on Y, 0.760
+# $$    M2: effect (b path) of mediator 2 on Y, 0.376
+# $$    X: direct effect (c path) of independent variable on Y, -0.016
+# $$    P1: effect of covariate on Y, 0.224
+#
+# $$ Mediator (second section) shows the a paths of each mediator
+# $$    M1 ~ X: effect on mediator 1 (a path), 0.648
+# $$    M2 ~ X: effect on mediator 2 (a path), 0.071
+#
+# $$ Defined parameters includes the indirect effects (ab paths), direct 
+# $$ effect (c' path), and total effects (sum of c' and all ab paths)
+# $$    ab1: indirect effect of X via mediator 1, 0.493
+# $$         This is the product of a1 and b1 from above, 0.648 * 0.760 = 0.493
+# $$    ab2: indirect effect of X via mediator 2, 0.027
+# $$         This is the product of a2 and b2 from above, 0.071 * 0.376 = 0.027
+# $$    total: total effect on Y of independent variable and mediators, 0.504; 
+# $$         -0.016 + (0.648 * 0.760) + (0.071 * 0.376) = -0.016 + 0.493 + 0.027
+# $$          note it does not include effect of covariate P1 on Y
+#
+# Regressions:
+#                  Estimate  Std.Err  z-value  P(>|z|)
+# Y ~                                                 
+#   M1        (b1)    0.760    0.088    8.632    0.000
+#   M2        (b2)    0.376    0.083    4.508    0.000
+#   X          (c)   -0.016    0.108   -0.146    0.884
+#   P1                0.224    0.101    2.209    0.027
+# M1 ~                                                
+#   X         (a1)    0.648    0.104    6.261    0.000
+# M2 ~                                                
+#   X         (a2)    0.071    0.111    0.645    0.519
+# 
+# Variances:
+#                  Estimate  Std.Err  z-value  P(>|z|)
+#  .Y                 0.828    0.117    7.071    0.000
+#  .M1                1.070    0.151    7.071    0.000
+#  .M2                1.228    0.169    7.071    0.000
+# 
+# Defined Parameters:
+#                  Estimate  Std.Err  z-value  P(>|z|)
+#  ab1               0.486    0.097    4.990    0.000
+#  ab2               0.038    0.042    0.890    0.373
+#  total             0.508    0.128    3.952    0.000
+#  direct           -0.016    0.108   -0.146    0.884
+
--- mock-data-creation.R
+++ mock-data-creation.R
@@ -33,37 +33,37 @@ bounded_norm <- function(n, mean, sd, lower = -Inf, upper = Inf) {
   return(x)
 }
 
-# Polygenic risk score
+# Polygenic risk score (Independent)
 PRS <- bounded_norm(n = sample_size,
                     mean = -0.019,
                     sd = 0.0817,
                     lower = -0.3,
                     upper = 0.3)
   
-# Early depression
+# Early depression (Covariate)
 W1_dep <- bounded_norm(n = sample_size,
                        mean = 11.38,
                        sd = 7.61,
                        lower = 0,
                        upper = 56)
 
-# Education, ordinal, 13 levels
+# Education (Mediator)
 Education <- sample(x = ordered(x = 1:13), 
                     size = sample_size, 
                     replace = TRUE)
 
-# Health Insurance
+# Health Insurance (Mediator)
 Health_ins <- bounded_norm(n = sample_size,
                            mean = 9.25,
                            sd = 4.599,
                            lower = 0,
                            upper = 12)
 
-# Total assets
+# Total assets (Mediator)
 Assets <- sample(x = ordered(x = 1:9),
                  size = sample_size,
                  replace = TRUE)
-# Debt
+# Debt (Mediator)
 Debt <- sample(x = ordered(x = 1:3),
                size = sample_size,
                replace = TRUE)
@@ -80,12 +80,29 @@ triads <- max(dyads) + rep(x = 1:27, times = 3)
 quads <- max(triads) + rep(x = 1:1, times = 4)
 FamilyID <- c(singletons, dyads, triads, quads)
 
-mock_data <- data.frame(PRS = PRS,
-                        W1_dep = W1_dep,
-                        Education = Education,
-                        Health_ins = Health_ins,
-                        Assets = Assets,
-                        Debt = Debt,
+# Need to simulate dependent variable
+# Some correlation estimates between PRS and mediators:
+# Education: -0.115
+# Health_ins: -0.060
+# Assets: -0.084
+# Debt: 0.082
+# W4_dep ~ PRS + W1_dep + Education + Health_ins + Assets + Debt
+# Look at education: slight negative relationship between Education and PRS
+# Higher education score, lower PRS, so want to modify probabilities of 
+# Education based on PRS score. PRS ranges -0.3 to 0.3, can make a vector of 
+# probabilities (prob argument of sample need not sum to one) of appropriate 
+# length (in Education case, length = 13)
+
+
+# W4_dep	W4_Dep
+# 2.64	2.57	dependent	Continuous	0-15
+
+mock_data <- data.frame(DepPRSIN = PRS,
+                        CESDW1 = W1_dep,
+                        H4ED2 = Education,
+                        H4HS3 = Health_ins,
+                        H4EC7 = Assets,
+                        H4EC8 = Debt,
                         FamilyID = sample(x = FamilyID, size = sample_size))
 
 write.csv(x = mock_data, file = "mock_data.csv", row.names = FALSE)
